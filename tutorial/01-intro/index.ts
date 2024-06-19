8090bad4f4e1ef45d14445cb902521e55116c364
tten__wi <utenvitedsonthi@gmail.com>
2024-06-19 07:32:36 +0700

    type

--- tutorial/01-intro/index.ts
+++ tutorial/01-intro/index.ts
@@ -1,16 +1,17 @@
 import { sleep } from "bun";
+import type { Browser, Page } from "puppeteer";
 import puppeteerExtra from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 puppeteerExtra.use(StealthPlugin());
 
-async function run() {
-  const brower = await puppeteerExtra.launch({
+async function run(): Promise<void> {
+  const brower: Browser = await puppeteerExtra.launch({
     headless: false,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
-  const page = await brower.newPage();
+  const page: Page = await brower.newPage();
 
   await page.goto("https://www.google.com");
   await sleep(100);
